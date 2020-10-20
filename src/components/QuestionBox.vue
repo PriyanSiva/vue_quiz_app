42e30908e40749ee8a0670448b16772efaf57fd3
priyansiva <priyansiva1995@gmail.com>
2020-10-20 14:04:45 +0530

    initial commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="question-box-container">
+        <b-jumbotron>
+            <template #lead>
+            Some questions here!
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            Answer list
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
